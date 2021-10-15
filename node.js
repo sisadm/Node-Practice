8a1c34c50ab469cadf28f85beec30abd83f66205
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-10-15 21:21:13 +0100

    cut all require and paste to profile.js
    add require to profile.js

--- node.js
+++ node.js
@@ -3,62 +3,11 @@
 // Parse the data
 // Print the data
 
-// Require  https module
-const https = require('https');
+const profile = require('./profile.js');
 
-// Require http module for status code
-const http = require('http');
 
-// Print Error Message
-function printError(error) {
-    console.error(error.message);
-}
 
-//Function to print message to console
-function printMessage(username, badgeCount, points) {
-    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-    console.log(message);
-} 
-
-function getProfile(username) {
-
-    try {
-        // connect to the API URL(https://teamtreehouse.com/${username}.json) 
-        const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-            if(response.statusCode === 200) {
-                let body = '';
-                // Read the data
-                response.on('data', data => {
-                    body += data.toString();
-                })
-
-                response.on('end', () => {
-                    try {
-                        //Parse the data
-                        const profile = JSON.parse(body);
-                        printMessage(username, profile.badges.length, profile.points.JavaScript);  
-                    } catch(error) {
-                        printError(error);
-                    }
-                });
-            } else {
-                const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
-                const statusCodeError = new Error(message);
-                printError(statusCodeError); 
-            }
-        });
-        // Error handle
-        request.on('error', printError);
-    } catch (error) {
-        printError(error);
-    }
-}
-
-console.log(process.argv);
 const users = process.argv.slice(2);
-
-
-
 
 // const users = ['chalkers', 'alenaholligan', 'davemcfarland'];
 
@@ -68,4 +17,4 @@ const users = process.argv.slice(2);
 
 // simple forEach because getProfile need 1 parameter 
 
-users.forEach(getProfile);
+users.forEach(profile.get);
