03e1df611559c987dad3428d89747c9b7e34e36f
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-10-26 20:42:45 +0100

    adding root route for request

--- express/express.js
+++ express/express.js
@@ -2,4 +2,10 @@ const express = require ('express');
 
 const app = express();
 
+
+// adding root route for request from user
+app.get('/', (request, response)=> {
+    response.send('Hello World!');
+});
+
 app.listen(3000);
