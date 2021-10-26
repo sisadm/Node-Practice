26925eebc71dc93ac04bcb86cd6140bf39e70da2
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-10-26 21:00:22 +0100

    adding /hello routs for get method

--- express/express.js
+++ express/express.js
@@ -8,6 +8,11 @@ app.get('/', (req, res)=> {
     res.send('<h1>Hello World!</h1>');
 });
 
+
+app.get('/hello', (req, res)=> {
+    res.send('<h1>Hello From the Otherside!</h1>');
+});
+
 app.listen(3000, () => {
     console.log('the application is running on localhost:3000')
 });
