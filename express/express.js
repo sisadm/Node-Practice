159fd11c475ee8809530eb40773e6139d05bbadb
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-10-26 20:58:47 +0100

    adding second parameter for listen method

--- express/express.js
+++ express/express.js
@@ -8,4 +8,6 @@ app.get('/', (req, res)=> {
     res.send('<h1>Hello World!</h1>');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('the application is running on localhost:3000')
+});
