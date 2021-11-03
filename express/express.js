bd31170ff9df5684928631f2e28d82cfd2623812
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-11-03 21:32:40 +0000

    save the entered value into Cookie

--- express/express.js
+++ express/express.js
@@ -30,6 +30,7 @@ app.get('/hello', (req, res)=> {
 })
 
 app.post('/hello', (req, res)=> {
+    res.cookie('username', req.body.username);
     res.render('hello', { name: req.body.username});
     
 })
