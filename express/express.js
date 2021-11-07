42bb12588534ce9846c51b1466149554271b1b0a
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-11-07 19:31:46 +0000

    create a separate folder and file for routes
    cut out and paste the routes from express.js to index.js

--- express/express.js
+++ express/express.js
@@ -41,40 +41,6 @@ app.use((req, res, next) => {
 })
 
 
-
-// adding root route for request from user
-app.get('/', (req, res)=> {
-    const name = req.cookies.username
-    if(name) {
-        res.render('index', {name});
-    } else {
-        res.redirect('/hello');
-    }
-});
-
-app.get('/cards', (req, res)=> {
-    res.render('card', { prompt: 'Who is buried in Grants tomb?'});
-});
-
-app.get('/hello', (req, res)=> {
-    const name = req.cookies.username;    
-    if(name) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
-})
-
-app.post('/hello', (req, res)=> {
-    res.cookie('username', req.body.username);
-    res.redirect('/');    
-})
-
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
-
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
---
+++ express/routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+// adding root route for request from user
+app.get('/', (req, res)=> {
+    const name = req.cookies.username
+    if(name) {
+        res.render('index', {name});
+    } else {
+        res.redirect('/hello');
+    }
+});
+
+app.get('/cards', (req, res)=> {
+    res.render('card', { prompt: 'Who is buried in Grants tomb?'});
+});
+
+app.get('/hello', (req, res)=> {
+    const name = req.cookies.username;    
+    if(name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
+})
+
+app.post('/hello', (req, res)=> {
+    res.cookie('username', req.body.username);
+    res.redirect('/');    
+})
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
+});
