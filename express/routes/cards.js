05ba4e7bf92b48774b315705875ef5651acf024f
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-11-07 20:36:54 +0000

    cards.js

--- express/routes/cards.js
+++ express/routes/cards.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=> {
-    res.render('card', { prompt: 'Who is buried in Grants tomb?'});
+router.get('/', (req, res) => {
+    res.render('card', { prompt: "Who is buried in Grant's tomb?" });
 });
 
 module.exports = router;
