db9ef6514dde0e2e096ca7cf76582d477575e114
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-11-13 20:34:09 +0000

    delete one line

--- express/routes/cards.js
+++ express/routes/cards.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json')
 const { cards } = data;
 
+
+
+
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
     const text = cards[id][side];
     let { hint } = cards[id];
     const templateData = {text, hint, id, side};
+    console.log(templateData);
     if(side == 'answer') { 
         templateData.side = 'question';
         templateData.hint = '';
@@ -16,11 +20,7 @@ router.get('/:id', (req, res) => {
     } else if (side == 'question') {
         templateData.side = 'answer';
         res.render('card', templateData);
-    }
-
-    res.render('card', templateData); 
-    
-    
+    }  
 });
 
 module.exports = router;
