1c19f9fa4dcfeb88fa6570a694b058b5936e9579
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-11-17 20:57:23 +0000

    create a folder and css to out App

--- express/express.js
+++ express/express.js
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
+app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
