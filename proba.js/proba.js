a4c09b895486f7b1a3b00b89ec6fc14a3a4d1b01
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2022-04-12 21:17:42 +0100

    add arr1 sort it

--- proba.js/proba.js
+++ proba.js/proba.js
@@ -1,1 +1,5 @@
-console.log('hello');
+let arr1 = [357, 337, 344, 402, 376, 398, 304, 368, 656, 382, 310, 354, 387, 391, 366, 376, 316, 349, 392, 386];
+
+arr1.sort(function(a, b) {return a-b});
+
+console.log(arr1);
