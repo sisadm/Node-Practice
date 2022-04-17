df886ebf6823eaec6821ba7d0fdf2f356b01fd6b
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2022-04-17 12:04:47 +0100

    update to to know when an array contains even length

--- proba.js/proba.js
+++ proba.js/proba.js
@@ -1,4 +1,9 @@
-let arr1 = [357, 337, 344, 402, 376, 398, 304, 368, 656, 382, 310, 354, 387, 391, 366, 376, 316, 349, 392, 386];
+// let arr1 = [357, 337, 344, 402, 376, 398, 304, 368, 656, 382, 310, 354, 387, 391, 366, 376, 316, 349, 392, 386, 222];
+
+
+ let arr1 = [1, 2, 3, 4, 5];
+
+
 // let arr1Correct = [357, 337, 344, 402, 376, 398, 304, 368, 356, 382, 310, 354, 387, 391, 366, 376, 316, 349, 392, 386];
 
 
@@ -25,6 +30,7 @@ function SumAndDivide(arr) {
     // using for loop summarizing the numbers from array to a variable and divide the length of array to get median 
     let arr1Sum = 0;
     let arr1Median;
+    let arrQ1;
     
     //console.log(`no value variable : ${arr1Sum}`);
     for(let item of arr ) {
@@ -39,12 +45,15 @@ function SumAndDivide(arr) {
             return (arr[arr.length / 2] + arr[(arr.length / 2) - 1]) / 2;
         } 
         else {
-            return (arr[arr.length - 1] / 2);
+            return (arr[(arr.length - 1) / 2]);
         };
     };
 
     SetTime('Median for the array', arr1Median(arr1), 3000);
 };
+
+// lower Quarter 
+
 
 
 
